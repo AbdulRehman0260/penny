bc77d44da7556482f92d70bc91ae9f0d644dc25d
Abdul Rehman Shoukat <abdulzain19@gmail.com>
2025-08-02 13:43:52 +0100

    frontend netlify adjustments

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_URL;
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: `${API_BASE}/api`,
   withCredentials: true,
 });
